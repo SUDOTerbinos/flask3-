f8efb55ce036afc53791ee9b746b722afcf2c11e
SUDOTerbinos <terbinosabebebirbirsa@gmail.com>
2025-01-03 17:10:55 +0300

    first commit

--- python.py
+++ python.py
@@ -3,17 +3,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def check_number():
     if request.method == 'POST':
-        name = request.form.get('name')
-        if name:
-            return f"Hello, {name}!"
+        number = request.form.get('number')
+        if number and number.isdigit():
+            number = int(number)
+            result = "even" if number % 2 == 0 else "odd"
+            return f"The number {number} is {result}."
         else:
-            return "Please provide a name."
+            return "Please enter a valid number."
     return '''
         <form method="post">
-            Enter your name: <input type="text" name="name">
-            <input type="submit" value="Submit">
+            Enter a number: <input type="text" name="number">
+            <input type="submit" value="Check">
         </form>
     '''
 
